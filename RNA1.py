12edaeaaf7d9fc29f083e3afbf8b78dcf91dd14b
Mario Alexis Ramirez <malexis_2014@outlook.com>
2022-09-12 20:16:59 -0500

    Después de muchas, muchas horas de pensar y buscar, encontré una forma de cómo implementar la función de pérdida de cross_entropy (en la bibliografía del curso) la implementación no me pareció para nada trivial, de hecho al intentar correrla tuve muchos problemas. Cuando por fin corrio esta última modificación, seleccioné 71 epocas, mini_batch de 10, eta de 3.5, los resultados se ven en la captura número 005.png donde se observa que la función de costo para el conjunto de     entrenamiento esta bastante bien con un valor cercano a cero, la     exactitud en el conjunto de entrenamiento es perfecta, sin embargo,     en el conjunto de validación la función de costo es bastante alta lo     cual significa que la red no está aprendiendo :( La forma de implementar la función croos-entropy aparece en el file NET.py tomada de la referencia del curso.

--- RNA1.py
+++ RNA1.py
@@ -1,11 +1,11 @@
 import mnist_loader
-import network
+import Network
 import pickle
 training_data, validation_data , test_data = mnist_loader.load_data_wrapper()
 training_data = list(training_data)
 test_data = list(test_data)
-net=network.Network([784,35,10])
-net.SGD( training_data, 250, 10, 2.5, test_data=test_data)
+net=Network.Network([784,30,10], cost=Network.CrossEntropyCost)
+net.SGD( training_data[:1000], 71, 10, 3.5, evaluation_data=test_data, lmbda=0.1, monitor_evaluation_cost=True, monitor_evaluation_accuracy=True, monitor_training_cost=True, monitor_training_accuracy=True)
 archivo = open("red_prueba1.pkl",'wb')
 pickle.dump(net,archivo)
 archivo.close()
@@ -14,8 +14,7 @@ exit()
 archivo_lectura = open("red_prueba.pkl",'rb')
 net = pickle.load(archivo_lectura)
 archivo_lectura.close()
-net.SGD( training_data, 10, 50, 4, test_data=test_data)
-model.compile(optimizer=optimizers.ADAM()
+net.SGD( training_data, 71, 10, 3.5, test_data=test_data)
 archivo = open("red_prueba.pkl",'wb')
 pickle.dump(net,archivo)
 archivo.close()
